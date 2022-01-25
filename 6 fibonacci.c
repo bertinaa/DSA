66cc87d006bc4f051fdac561157380b43de1cb8c
Bertina S <76837815+bertinaa@users.noreply.github.com>
2022-01-25 20:09:08 +0530

    Update 6 fibonacci.c

--- 6 fibonacci.c
+++ 6 fibonacci.c
@@ -1,6 +1,18 @@
+//using recursion
 int fib(int n)
 {
 if(n==0 || n==1)
     return n;
  return fib(n-1)+fib(n-2);
+}
+
+
+int fib(int n)
+{
+int f[n+1];
+    f[0]=0;
+    f[1]=1;
+    for(int i =2; i<=2; i++)
+        f[i] = f[i-1] + f[i-2];
+    return f[n];
 }
