90dedc72cd180754fec300d0ab5721456ae32935
Bertina S <76837815+bertinaa@users.noreply.github.com>
2022-01-26 11:06:11 +0530

    Create 1 ArrayList.java

---
+++ Arrays/1 ArrayList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+class Test
+{
+  public static void main(String args[])
+  {
+  ArrayList<Integer> al = new ArrayList<Integer>(); // putting a number inside () will make the array static
+    al.add(10);
+    al.add(20);
+    System.out.print(al);
+  }
+}
