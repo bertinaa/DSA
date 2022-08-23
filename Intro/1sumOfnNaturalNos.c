7976bd18ed21d8b6616e9e51d5b237e1e5bc37f4
Bertina S <76837815+bertinaa@users.noreply.github.com>
2022-08-23 17:24:24 +0530

    Create 1sumOfnNaturalNos.c

---
+++ Intro/1sumOfnNaturalNos.c
@@ -0,0 +1,24 @@
+
+int fun1(int n){
+    return n*(n+1)/2;
+}
+
+
+int fun2(int n){
+    int sum = 0;
+    for(int i=1;i<=n;i++){
+        sum = sum + i;
+    }
+    return sum;
+}
+
+
+int fun3(int n){
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        for(int j = 1;j<=i;j++){
+            sum++;
+          //(1)+(1+1)+(1+1+1)+...
+        }
+    }return sum;
+}
